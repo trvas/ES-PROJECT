4d943a94e683c17178c6119fbf4cdd53bcdf448b
glrss-iscte <glrss@iscte-iul.pt>
2021-10-01 17:19:03 +0100

    x

--- es_proj/test.java
+++ es_proj/test.java
@@ -3,7 +3,7 @@ public class test {
     public static void main(String[] args){
         System.out.println("hi there!!!");
         System.out.println("buh byeeee!");
-	System.out.println("Aw Hell No");
-
+	    System.out.println("Aw Hell No");
+        System.out.println("lol");
     }    
 }
