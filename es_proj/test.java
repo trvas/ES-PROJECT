3005c636c7a83e23d42155bd9cbc7f5570623925
trvas <trvas@iscte-iul.pt>
2021-10-01 16:49:03 +0100

    branches

--- es_proj/test.java
+++ es_proj/test.java
@@ -1,6 +1,8 @@
 package es_proj;
 public class test {
     public static void main(String[] args){
-        System.out.println("hi there 123" + "Hello World");
+        System.out.println("hi there!!!");
+        System.out.println("buh byeeee!");
+
     }    
 }
