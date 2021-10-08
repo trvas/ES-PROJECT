9f099cc1ff43a9fcb6a6ac5b02b002f7a3a5df3f
glrss-iscte <glrss@iscte-iul.pt>
2021-10-08 16:46:23 +0100

    ss

--- es_proj/frame.java
+++ es_proj/frame.java
@@ -1,16 +1,11 @@
 package es_proj;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 
 public class frame {
     public static void main(String[] args){
        
         JFrame frame = new JFrame();
-        JLabel label = new JLabel();
-        ImageIcon image = new ImageIcon("dog.jpg");
-        label.setIcon(image);
 
         frame.setTitle("Project Manager");
         frame.setVisible(true);
