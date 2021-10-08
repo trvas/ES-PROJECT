298114b66540e884b15c23b28682eefea8eb1acc
glrss-iscte <glrss@iscte-iul.pt>
2021-10-08 15:41:50 +0100

    Jframe testes

--- es_proj/test.java
+++ es_proj/test.java
@@ -1,9 +1,13 @@
 package es_proj;
+import javax.swing.JFrame;
+
 public class test {
     public static void main(String[] args){
-        System.out.println("hi there!!!");
-        System.out.println("buh byeeee!");
-	    System.out.println("Aw Hell No");
-        System.out.println("lol");
+       
+        JFrame frame = new JFrame();
+        frame.setTitle("Project Manager");
+        frame.setVisible(true);
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //sair da app 
     }    
 }
