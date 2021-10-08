f0e1444b623308a69b4e30dfe50b558f7eb14edc
glrss-iscte <glrss@iscte-iul.pt>
2021-10-08 16:41:07 +0100

    teste

--- es_proj/test.java
+++ es_proj/test.java
@@ -1,13 +1,7 @@
 package es_proj;
-import javax.swing.JFrame;
 
-public class test {
+public class test extends frame{
     public static void main(String[] args){
-       
-        JFrame frame = new JFrame();
-        frame.setTitle("Project Manager");
-        frame.setVisible(true);
-        frame.setSize(500,500);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //sair da app 
+        frame.main(args);
     }    
 }
